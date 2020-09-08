607c8012178c2257a5050543c83cf121ed2ee728
lisambamberg <lisambamberg@gmail.com>
2020-09-08 11:46:22 -0500

    Removed uppercase method where not applicable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log("Lisa Bamberg".toUpperCase());
-console.log("Frontend software developer".toUpperCase());
-console.log("I am from Birmingham, AL. I am currently attending Innovate Bham's Full Stack Software Development course.".toUpperCase());
-console.log("My interests:\n Technology \n Art \n Culture".toUpperCase());
-console.log("My previous work experience: RxBenefits - Data Integration Analysis - Integrated client's data into database".toUpperCase());
-console.log("My skills: \n JavaScript \n CSS/ HTML \n BAM: Violin \n BAM: Drawing".toUpperCase());
+console.log("Frontend software developer");
+console.log("I am from Birmingham, AL. I am currently attending Innovate Bham's Full Stack Software Development course.");
+console.log("My interests:\n Technology \n Art \n Culture");
+console.log("My previous work experience: RxBenefits - Data Integration Analysis - Integrated client's data into database");
+console.log("My skills: \n JavaScript \n CSS/ HTML \n BAM: Violin \n BAM: Drawing");
