7cc4952fa0b45562599c6a936b50103463f28e15
lisambamberg <lisambamberg@gmail.com>
2020-09-08 12:02:59 -0500

    Added displayPosition function

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 console.log("Lisa Bamberg".toUpperCase());
 console.log("Frontend software developer");
 console.log("I am from Birmingham, AL. I am currently attending Innovate Bham's Full Stack Software Development course.");
-console.log("My interests:\n Technology \n Art \n Culture");
-console.log("My previous work experience: RxBenefits - Data Integration Analysis - Integrated client's data into database");
+console.log("My Interests:\n Technology \n Art \n Culture");
+
+let name = "RxBenefits - ";
+let title = "Data Integration Analysis - ";
+let description = "Integrated client's data into database";
+console.log("My Previous Experience:");
+function displayPosition () {
+    console.log(name, title, description);
+}
+displayPosition();
+
 console.log("My skills: \n JavaScript \n CSS/ HTML \n BAM: Violin \n BAM: Drawing");
+
+
+
+
+
