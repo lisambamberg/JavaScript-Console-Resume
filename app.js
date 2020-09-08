dafb60941b76996f8bdc7c8e707e16727d506971
lisambamberg <lisambamberg@gmail.com>
2020-09-08 13:03:30 -0500

    Refactored displayPosition function

--- app.js
+++ app.js
@@ -2,19 +2,16 @@ console.log("Lisa Bamberg".toUpperCase());
 console.log("Frontend software developer");
 console.log("I am from Birmingham, AL. I am currently attending Innovate Bham's Full Stack Software Development course.");
 console.log("My Interests:\n Technology \n Art \n Culture");
-
-let name = "RxBenefits - ";
-let title = "Data Integration Analysis - ";
-let description = "Integrated client's data into database";
 console.log("My Previous Experience:");
-function displayPosition () {
-    console.log(name, title, description);
+
+function displayPosition(name, title, description) {
+    console.log(name + title + description);
 }
-displayPosition();
+
+let job1 = displayPosition("RxBenefits - ", "Data Integration Analysis - ", "Integrated client's data into database");
+let job2 = displayPosition("UAB Computer Science Camp - ", "Facilitor - ", "Assisted in teaching kids computer science");
 
 console.log("My skills: \n JavaScript \n CSS/ HTML \n BAM: Violin \n BAM: Drawing");
-
-
 
 
 
