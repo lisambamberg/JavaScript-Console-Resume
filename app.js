af2de3d34f1ba63202792a25198117ecc76b9524
lisambamberg <lisambamberg@gmail.com>
2020-09-08 13:18:17 -0500

    Added displaySkill function

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ console.log("My Previous Experience:");
 function displayPosition(name, title, description) {
     console.log(name + title + description);
 }
-
 let job1 = displayPosition("RxBenefits - ", "Data Integration Analysis - ", "Integrated client's data into database");
 let job2 = displayPosition("UAB Computer Science Camp - ", "Facilitor - ", "Assisted in teaching kids computer science");
 
-console.log("My skills: \n JavaScript \n CSS/ HTML \n BAM: Violin \n BAM: Drawing");
+console.log("My skills:");
 
-
-
+function displaySkill(name, bool) {
+    if (bool == true) {
+        console.log("BAM:" + name);
+    } else {
+        console.log(name);
+    }
+}
+let skills1 = displaySkill("JavaScript", false);
+let skills2 = displaySkill("HTML/CSS", false);
+let skills3 = displaySkill("Violin", true);
+let skills4 = displaySkill("Drawing", true);
