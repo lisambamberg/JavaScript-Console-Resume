cc42647d3aae542f06dbcb7675dd5e0bf91ce9bb
lisambamberg <lisambamberg@gmail.com>
2020-09-08 11:39:57 -0500

    Completed objectives section of assignment

---
+++ app.js
@@ -0,0 +1,6 @@
+console.log("Lisa Bamberg");
+console.log("Frontend software developer");
+console.log("I am from Birmingham, AL. I am currently attending Innovate Bham's Full Stack Software Development course.");
+console.log("My interests:\n Technology \n Art \n Culture");
+console.log("My previous work experience: RxBenefits - Data Integration Analysis - Integrated client's data into database");
+console.log("My skills: \n JavaScript \n CSS/ HTML \n BAM: Violin \n BAM: Drawing");
